c42d9b4c9916bff5456a743e8b3d046053162861
Jose V <jose@lamproslabs.com>
2024-01-11 13:41:17 -0700

    First Commit

---
+++ FreshVotes/src/main/java/com/freshvotes/domain/Vote.java
@@ -0,0 +1,29 @@
+package com.freshvotes.domain;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+
+@Entity
+public class Vote {
+	
+	// Obj that contains the foreign keys
+	private VoteId pk;
+	private Boolean upvote;
+	
+	@EmbeddedId
+	public VoteId getPk() {
+		return pk;
+	}
+
+	public void setPk(VoteId pk) {
+		this.pk = pk;
+	}
+
+	public Boolean getUpvote() {
+		return upvote;
+	}
+
+	public void setUpvote(Boolean upvote) {
+		this.upvote = upvote;
+	}
+}
